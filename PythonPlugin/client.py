b840aef97845ea390e56377d08284318d5baf1a7
Weilian Song <weiliansong@hotmail.com>
2016-04-10 07:32:48 -0400

    2nd push of python code

--- PythonPlugin/client.py
+++
@@ -1,6 +0,0 @@
-import socket, ssl, json
-
-
-
-client = Client()
-client.connect("10.20.221.152", 9012)
--- PythonPlugin/controller.py
+++ PythonPlugin/controller.py
@@ -1,12 +1,4 @@
-#!/usr/bin/python
-
-import sys
-sys.path.append('/usr/bin/python3.4')
-import sublime
-import sublime_plugin
-import threading
-import socket, json
-import ssl 
+import sublime, sublime_plugin, threading, socket, json, ssl, re, hashlib
 
 class ModificationListener(sublime_plugin.EventListener):
 	prev_pos = {}
@@ -15,12 +7,22 @@ class ModificationListener(sublime_plugin.EventListener):
 		view_id = view.id()
 		sel = view.sel()[0].begin()
 		if view.file_name() != None:
-			file_name = view.file_name().split("/")[-1]
 			ML = ModificationListener
+
+			current_file = view.file_name()
+			if current_file[0] != '/':
+				sub_string = current_file[4:]
+				self.parsed_path = sub_string.split('\\')
+			else:
+				self.parsed_path = current_file.split('/')
+
 			if sel > ML.prev_pos[view_id]:
 				region = sublime.Region(ML.prev_pos[view_id], sel)
+				handler.sendInsertion(view.substr(region),
+					region, self.parsed_path[-1])
 			else:
 				region = sublime.Region(sel, ML.prev_pos[view_id])
+				handler.sendDeletion(region, self.parsed_path[-1])
 
 	def on_activated(self, view):
 		view_id = view.id()
@@ -36,34 +38,57 @@ class ModificationListener(sublime_plugin.EventListener):
 			ML.prev_pos[view_id] = sel.begin()
 
 class connectServerCommand(sublime_plugin.TextCommand):
-	address = ''
-	pwd = ''
 
 	def run(self, view):
-		self.view.window().show_input_panel("Address Input:",
-			"0.0.0.0", self.on_done1, None, None)
+		self.view.window().show_input_panel("Username Input:",
+			"", self.on_done1, None, None)
+
 	def on_done1(self, user_input):
-		self.address = user_input
+		self.username = user_input
 		self.view.window().show_input_panel("Address Input:",
-			"", self.on_done2, None, None)
+			"0000", self.on_done2, None, None)
+
 	def on_done2(self, user_input):
-		self.pwd = user_input
-		if handler.connect(self.address, self.pwd) != True:
+		self.server_id = user_input
+		if re.match("\w{4}", self.server_id) != None:
+			self.view.window().show_input_panel("Password Input:",
+				"", self.on_done3, None, None)
+		else:
+			sublime.error_message("Invalid Server ID")
+	def on_done3(self, user_input):
+		self.pwd = hashlib.sha224(user_input.encode('utf-8')).hexdigest()
+		if handler.connect() != True:
 			sublime.error_message("Unable to connect to server")
 		else:
-			sublime.status_message("Connected")
-			handler.startThread()
+			if handler.sendLogin(self.server_id, self.pwd, self.username) == True:
+				sublime.status_message("Connected")
+				handler.startThread()
+			else:
+				sublime.error_message("Wrong Login Information")
 
 class killConnectionCommand(sublime_plugin.TextCommand):
 	def run(self, view):
 		handler.closeConnections()
 
+class addProjectCommand(sublime_plugin.TextCommand):
+	def run(self, view):
+		current_project = self.view.window().project_file_name()
+		if current_project[0] != '/':
+			sub_string = current_project[4:]
+			self.parsed_path = sub_string.split('\\')
+		else:
+			self.parsed_path = sub_string.split('/')
+
+		Handler.addProject(self.parsed_path)
+
 class Handler(object):
+	current_project = ""
+
 	def __init__(self):
 		self.client = Client()
 
-	def connect(self, address, pwd):
-		return self.client.connect(address, pwd)
+	def connect(self):
+		return self.client.connect()
 
 	def startThread(self):
 		self.client.startReadThread()
@@ -71,14 +96,37 @@ class Handler(object):
 	def closeConnections(self):
 		self.client.closeConnections()
 
+	def setProject(self, t_project_name):
+		self.current_project = t_project_name
+		self.client.setProject(project_name)
+
+	def sendInsertion(self, insertion, position, filename):
+		self.client.sendInsertion(insertion, position, filename)
+
+	def sendDeletion(self, position, filename):
+		self.client.sendDeletion(position, filename)
+
+	def sendLogin(self, t_server_id, t_pwd, t_username):
+		self.client.login(t_server_id, t_pwd, t_username)
+
+	def localInsert(self, json_msg):
+		parsed_json = json.loads(json_msg)
+		
+		insertion = parsed_json['insertion']
+		position = parsed_json['position']
+		username = parsed_json['username']
+		filename = parsed_json['filename']
+
 class Client(object):
-	session_id = ''
+	proxy_ip = "10.20.216.10"
+	server_id = ''
 	port = 0
 	pwd = ''
+	username = ''
 
-	MSG_ID_LOGIN = 0
-	MSG_ID_NEW_FILE = 1
-	MSG_ID_INSERT = 2
+	MSG_ID_LOGIN = 0 # Finished
+	MSG_ID_NEW_FILE = 1 
+	MSG_ID_INSERT = 2 
 	MSG_ID_PULL_CHANGES = 3
 	MSG_ID_AVAILABLE_FILES = 4
 	MSG_ID_GET_FILES = 5
@@ -90,14 +138,12 @@ class Client(object):
 		# Require a certificate from the server
 		self.ssl_sock = ssl.wrap_socket(self.s, 
 			cert_reqs=ssl.CERT_NONE)
-		self.ssl_sock.settimeout(1.0)
+		self.ssl_sock.settimeout(1.5)
 
-	def connect(self, t_id, t_pwd):
-		self.session_id = t_id
-		self.port = 22753
-		self.pwd  = t_pwd
+	def connect(self):
+		self.port = 22754
 		try:
-			self.ssl_sock.connect((self.session_id, self.port))
+			self.ssl_sock.connect((self.proxy_ip, self.port))
 		except:
 			return False
 		else:
@@ -107,24 +153,37 @@ class Client(object):
 		self.ssl_sock.close()
 
 	def startReadThread(self):
-		self.read_thread = readThread(1, "Thread-1", this.ssl_sock)
+		self.read_thread = readThread(1, "Thread-1", self.ssl_sock)
+		self.read_thread.start()
 
-	def login(self):
+	def login(self, t_session_id, t_pwd, t_username):
+		self.session_id = t_session_id
+		self.pwd = t_pwd
+		self.username = t_username
 		msg = {
-			'ID': MSG_ID_LOGIN,
-			'session_id': session_id,
-			'pwd': pwd,
+			'ID': self.MSG_ID_LOGIN,
+			'server_id': self.session_id,
+			'pwd': self.pwd,
+			'username': self.username
 		}
 		
-		if self.ssl_sock.sendall(json.dumps(msg)) == None:
-			return False
-		else:
-			data = self.ssl_sock.recv(1024)
-			parsed_json = json.loads(data)
-			if parsed_json['yay'] == 1:
-				return True
+		self.ssl_sock.sendall(json.dumps(msg).encode('ascii'))
+		self.ssl_sock.flush()
+		chunks = ""
+		while True:
+			chunk = str(self.ssl_sock.recv(1))
+			if chunk.endswith("'"): chunk = chunk[:-1]
+			if chunk.startswith("b'"): chunk = chunk[2:]
+			if chunk != "\\n":
+				chunks += chunk 
 			else:
-				return False
+				break
+
+		parsed_json = json.loads(chunks)
+		if parsed_json['yay'] == 1:
+			return True
+		else:
+			return False
 
 	def killConnection(self):
 		self.ssl_sock.close()
@@ -141,21 +200,28 @@ class Client(object):
 		else:
 			return True
 
-	def sendCharEdit(self, char, pos, username, filename):
+	def sendInsertion(self, t_insertion, t_position, t_filename):
 		msg = {
-			'ID': MSG_ID_CHAR_EDIT,
-			'char': char,
-			'pos': pos,
-			'username': username,
-			'filename': filename,
+			'ID': self.MSG_ID_INSERT,
+			'insertion': t_insertion,
+			'position': t_position,
+			'username': self.username,
+			'filename': t_filename,
 		}
 
-		if self.ssl_sock.sendall(json.dumps(msg)) == None:
-			return False
-		else:
-			return True
+		self.ssl_sock.write((json.dumps(msg)).encode('ascii'))
+	
+	def sendDeletion(self, t_position, t_filename):
+		msg = {
+			'ID': self.MSG_ID_DELETE,
+			'position': t_position,
+			'username': self.username,
+			'filename': t_filename,
+		}
 
-	def addNewFile(self, filename, username):
+		self.ssl_sock.write((json.dumps(msg)).encode('ascii'))
+
+	def setProject(self, filename, username):
 		msg = {
 			'ID': MSG_ID_NEW_FILE,
 			'filename': filename,
@@ -176,11 +242,19 @@ class readThread(threading.Thread):
 	def run(self):
 		try:
 			while True:
-				data = self.ssl_sock.recv(1024)
-				if data != None:
-					print(data)
+				chunks = ''
+				while True:
+					chunk = str(self.ssl_sock.recv(1))
+					if chunk.endswith("'"): chunk = chunk[:-1]
+					if chunk.startswith("b'"): chunk = chunk[2:]
+					if chunk != "\\n":
+						chunks += chunk 
+					else:
+						break
+				handler.localInsert(chunks)
 		except:
-			print("Socket Error!")
+			print("Unexpected error", sys.exc_info()[0])
+			raise
 		else:
 			handler.closeConnections()
 
